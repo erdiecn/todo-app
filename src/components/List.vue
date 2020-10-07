cd91a43ab789e93a3e476d533534483b64ff4cad
Carmen E <erdiecn@utexas.edu>
2020-10-07 16:00:33 -0500

    First commit

---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>,
+        like the one you're currently reading
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
